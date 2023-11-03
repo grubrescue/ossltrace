ab948a7d9ef6ebb8190524e1b5b7ab40223e31ee
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-11-03 18:21:51 +0700

    a

--- src/lib.c
+++ src/lib.c
@@ -12,28 +12,10 @@
 
 #include <gnu/lib-names.h>
 
-typedef void * (*dlsym_callback)(void *restrict handle, const char *restrict symbol);
-dlsym_callback __real_dlsym = NULL;
-
-INPROC_SO_EXPORT
-void *
-dlsym(void *restrict handle, const char *restrict symbol) {
-    if (__real_dlsym == NULL) {
-        __real_dlsym = dlvsym(RTLD_DEFAULT, "dlsym", NULL); // todo
-        if (__real_dlsym == NULL) {
-            fprintf(stderr, "%s\n", dlerror());
-        }
-        assert(__real_dlsym != NULL);
-    }
-    fprintf(stderr, "%s is gonna be found\n", symbol);
-    __real_dlsym(handle, symbol);
-}
-
-
 typedef int (*SSL_write_callback)(SSL *ssl, const void *buf, int num);
-SSL_write_callback __real_SSL_write = NULL;
+static SSL_write_callback __real_SSL_write = NULL;
 
-INPROC_SO_EXPORT 
+INPROC_SYMBOL_VISIBLE 
 int 
 SSL_write(SSL *ssl, const void *buf, int num) {
     if (__real_SSL_write == NULL) {
@@ -56,9 +38,9 @@ SSL_write(SSL *ssl, const void *buf, int num) {
 
 
 typedef int (*SSL_read_callback)(SSL *ssl, void *buf, int num);
-SSL_read_callback __real_SSL_read = NULL;
+static SSL_read_callback __real_SSL_read = NULL;
 
-INPROC_SO_EXPORT 
+INPROC_SYMBOL_VISIBLE 
 int 
 SSL_read(SSL *ssl, void *buf, int num) {
     if (__real_SSL_read == NULL) {
@@ -66,12 +48,12 @@ SSL_read(SSL *ssl, void *buf, int num) {
     }
 
     int retval = __real_SSL_read(ssl, buf, num);
-    fprintf(stderr, "red bytes %d\n\n", retval);
+    // fprintf(stderr, "red bytes %d\n\n", retval);
 
-    if (retval > 0) {
-        write(2, buf, retval);
-    }
-    fprintf(stderr, "\n\n");
+    // if (retval > 0) {
+    //     write(2, buf, retval);
+    // }
+    // fprintf(stderr, "\n\n");
     
     // const uint8_t * charbuf = (const uint8_t*) buf;
     // for (int i = 0; i < num; i++) {
@@ -80,4 +62,29 @@ SSL_read(SSL *ssl, void *buf, int num) {
     // fprintf(stderr, "\ndone now\n\n\n");
 
     return retval;
+}
+
+typedef void * (*dlsym_callback)(void *restrict handle, const char *restrict symbol);
+static dlsym_callback __real_dlsym = NULL;
+
+INPROC_SYMBOL_VISIBLE
+void *
+dlsym(void *restrict handle, const char *restrict symbol) {
+    if (__real_dlsym == NULL) {
+        __real_dlsym = dlvsym(RTLD_DEFAULT, "dlsym", "GLIBC_2.34"); // todo версия???
+        if (__real_dlsym == NULL) {
+            fprintf(stderr, "%s\n", dlerror());
+            assert(0);
+        }
+    }
+
+    fprintf(stderr, "%s :: handle is %p, dlvsymmed symbol is %p\n", symbol, handle, SSL_read);
+
+    if (strcmp(symbol, "SSL_read")) {
+        return SSL_read;
+    } else if (strcmp(symbol, "SSL_write")) {
+        return SSL_write;
+    } else {
+        __real_dlsym(handle, symbol);
+    }
 }
