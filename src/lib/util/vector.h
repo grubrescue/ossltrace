a05492f1e88a1bcfeafae4737cc437ba1996319f
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-11-22 19:54:23 +0700

    реализация динамического массива

---
+++ src/lib/util/vector.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdlib.h>
+
+#define INPROC_VECTOR_DEFAULT_CAP 16U
+
+typedef struct vector{
+    void **arr;
+    size_t size;
+    size_t cap;
+} vector;
+
+// methods
+// nulls NOT supposed.
+void vector_init(vector *vec);
+void vector_init_cap(vector *vec, size_t cap);
+void *vector_push(vector *vec, void *value);
+void *vector_pop(vector *vec, void *value);
+size_t vector_size(vector *vec);
+void *vector_get(vector *vec, size_t idx);
+void vector_foreach(vector *vec, void (*callback_consumer)(void *item));
+void *vector_findfirst(vector *vec, int (*callback_predicate)(void *item));
+void vector_destroy(vector *vec);
+
+// implementation
+static void vector_ensure_size(vector *vec) {
+    if (vec->size == vec->cap) {
+        vec->cap = vec->cap * 3 / 2 + 1;
+
+        void **newptr = (void **) realloc(vec->arr, vec->cap);
+        if (newptr == NULL) {
+            perror("vector_ensure_size");
+            exit(EXIT_FAILURE);
+        }
+
+        vec->arr = newptr;
+    }
+}
+
+void vector_init_cap(vector *vec, size_t cap) {
+    vec->arr = (void**) malloc(cap * sizeof(void *));
+    vec->size = 0;
+    vec->cap = cap;
+}
+
+void vector_init(vector *vec) {
+    vector_init_cap(vec, INPROC_VECTOR_DEFAULT_CAP);
+}
+
+void *vector_push(vector *vec, void *value) {
+    vector_ensure_size(vec);
+    
+    vec->arr[vec->size++] = value;
+    return vec->arr[vec->size - 2];
+}
+
+void *vector_pop(vector *vec, void *value) {
+    return vec->arr[--vec->size];
+}   
+
+size_t vector_size(vector *vec) {
+    return vec->size;
+}
+
+void *vector_get(vector *vec, size_t idx) {
+    return vec->arr[idx];
+}
+
+void vector_foreach(vector *vec, void (*callback_consumer)(void *item)) {
+    for (int i = 0; i < vec->size; i++) {
+        callback_consumer(vec->arr[i]);
+    }
+}
+
+void *vector_findfirst(vector *vec, int (*callback_predicate)(void *item)) {
+    for (int i = 0; i < vec->size; i++) {
+        if (!callback_predicate(vec->arr[i])) {
+            return vec->arr[i];
+        }
+    }
+
+    return NULL;
+}
+
+void vector_destroy(vector *vec) {
+    free(vec->arr);
+    vec->arr = NULL;
+    vec->size = 0;
+    vec->cap = 0;
+}
