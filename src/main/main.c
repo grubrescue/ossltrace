bd8e4fad993059b8f54164e6515470e9119eda48
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-11-22 17:35:50 +0700

    обновление парсинга командной строки, первоначальное внедрение блеклиста

--- src/main/main.c
+++ src/main/main.c
@@ -13,33 +13,44 @@
 struct arguments {
     enum { PRELOAD, AUDIT, NAIVEPATCH } mode;
     char *output_file_path;
+    char *blacklist_file_path;
     char **child_argv;
 };
 
 void 
 print_usage(FILE *where, char *pathname) {
-    fprintf(where, "usage: %s -m|--mode: preload|audit|naivepatch> [command args]\n", pathname);
+    fprintf(where, "usage: %s [options] command [command args]\n", pathname);
+    fprintf(where, "check list of options by typing %s -h/--help\n", pathname);
+}
+
+void 
+print_help(FILE *where, char *pathname) {
+    print_usage(where, pathname);
+    fprintf(stdout, "no help yet, sorry!\n\n");
 }
 
 int 
 main(int argc, char **argv) {
     struct arguments arguments;
     arguments.output_file_path = NULL; 
-    arguments.child_argv = NULL; // required
+    arguments.child_argv = NULL; 
+    arguments.blacklist_file_path = NULL;
     arguments.mode = PRELOAD;
 
-    int finishedParsing = 0;
-    while (!finishedParsing) {
+    int finished_parsing = 0;
+    while (!finished_parsing) {
         int option_index = 0;
 
         static struct option long_options[] = {
-            { "method", required_argument, NULL, 'm' },
-            { "output", required_argument, NULL, 'o' },
-            { "filter", required_argument, NULL, 'f' },
-            { NULL,     0,                 NULL,  0  }
+            { "method",    required_argument, NULL, 'm' },
+            { "output",    required_argument, NULL, 'o' },
+            { "quiet",     no_argument,       NULL, 'q' },
+            { "blacklist", required_argument, NULL, 'b' },
+            { "help",      no_argument,       NULL, 'h' },
+            { NULL,        0,                 NULL,  0  }
         };
 
-        int c = getopt_long(argc, argv, "+m:o:f:", long_options, &option_index);
+        int c = getopt_long(argc, argv, "+m:o:qb:h", long_options, &option_index);
 
         switch (c) {
             case 'm':
@@ -56,14 +67,23 @@ main(int argc, char **argv) {
             case 'o':
                 arguments.output_file_path = strdup(optarg);
                 break;
-            case 'f':
-                printf("option f with value '%s'\n", optarg);
+            case 'q':
+                arguments.output_file_path = strdup("/dev/null");
+                break;
+            case 'b':
+                arguments.blacklist_file_path = strdup(optarg);
+                break;
+            case 'h':
+                print_help(stdout, argv[0]);
+                exit(EXIT_SUCCESS);
                 break;
             case '?':
                 print_usage(stderr, argv[0]);
                 exit(EXIT_FAILURE);
+                break;
             default:
-                finishedParsing = 1;
+                finished_parsing = 1;
+                break;
         }   
     }
 
@@ -74,56 +94,49 @@ main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
-    // pid_t pid = fork();
+    if (arguments.output_file_path != NULL) {
+        setenv(INPROC_LOG_OUTPUT_FILE_ENV_VAR, arguments.output_file_path, 1);
+        free(arguments.output_file_path);
+    } else {
+        unsetenv(INPROC_LOG_OUTPUT_FILE_ENV_VAR);
+    }
 
-    // if (pid == 0 /*child*/) {
-        if (arguments.output_file_path != NULL) {
-            setenv(INPROC_LOG_OUTPUT_FILE_ENV_VAR, arguments.output_file_path, 1);
-            free(arguments.output_file_path);
-        } else {
-            unsetenv(INPROC_LOG_OUTPUT_FILE_ENV_VAR);
-        }
+    if (arguments.blacklist_file_path != NULL) {
+        setenv(INPROC_BLACKLIST_FILE_ENV_VAR, arguments.blacklist_file_path, 1);
+        free(arguments.blacklist_file_path);
+    } else {
+        unsetenv(INPROC_BLACKLIST_FILE_ENV_VAR);
+    }
 
-        switch(arguments.mode) {
-            case PRELOAD:
-                char *preload_lib_path = getenv(INPROC_PRELOAD_ENV_VAR);
-                if (preload_lib_path == NULL) {
-                    preload_lib_path = INPROC_DEFAULT_PRELOAD_LIB_PATH;
-                }
-                setenv("LD_PRELOAD", preload_lib_path, 1);
-                break;
+    switch(arguments.mode) {
+        case PRELOAD:
+            char *preload_lib_path = getenv(INPROC_PRELOAD_ENV_VAR);
+            if (preload_lib_path == NULL) {
+                preload_lib_path = INPROC_DEFAULT_PRELOAD_LIB_PATH;
+            }
+            setenv("LD_PRELOAD", preload_lib_path, 1);
+            break;
 
-            case AUDIT:
-                char *audit_lib_path = getenv(INPROC_AUDIT_ENV_VAR);
-                if (audit_lib_path == NULL) {
-                    audit_lib_path = INPROC_DEFAULT_AUDIT_LIB_PATH;
-                }
-                setenv("LD_AUDIT", audit_lib_path, 1);
-                break;
-            case NAIVEPATCH:
-                char *naivepatch_lib_path = getenv(INPROC_NAIVEPATCH_ENV_VAR);
-                if (naivepatch_lib_path == NULL) {
-                    naivepatch_lib_path = INPROC_DEFAULT_NAIVEPATCH_LIB_PATH;
-                }
-                setenv("LD_PRELOAD", naivepatch_lib_path, 1);
-            default:
-        }
+        case AUDIT:
+            char *audit_lib_path = getenv(INPROC_AUDIT_ENV_VAR);
+            if (audit_lib_path == NULL) {
+                audit_lib_path = INPROC_DEFAULT_AUDIT_LIB_PATH;
+            }
+            setenv("LD_AUDIT", audit_lib_path, 1);
+            break;
+        case NAIVEPATCH:
+            char *naivepatch_lib_path = getenv(INPROC_NAIVEPATCH_ENV_VAR);
+            if (naivepatch_lib_path == NULL) {
+                naivepatch_lib_path = INPROC_DEFAULT_NAIVEPATCH_LIB_PATH;
+            }
+            setenv("LD_PRELOAD", naivepatch_lib_path, 1);
+        default:
+    }
 
-        if (execvp(arguments.child_argv[0], arguments.child_argv) == -1) {
-            perror(arguments.child_argv[0]);
-            exit(EXIT_FAILURE);
-        }
-    // } 
-    
-    // else if (pid > 0 /*parent*/) {
-    //     int status;
-    //     wait(&status);
-    //     printf("\n~ '%s' exited with return code %u ~\n\n", arguments.child_argv[0], WEXITSTATUS(status));
-    // } 
-    
-    // else {
-    //     perror("unforkable");
-    // }
-    
+    if (execvp(arguments.child_argv[0], arguments.child_argv) == -1) {
+        perror(arguments.child_argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     return EXIT_SUCCESS;
 }
--- src/main/main.h
+++ src/main/main.h
@@ -2,12 +2,12 @@
 
 #include "../common.h"
 
-// #define INPROC_DEFAULT_LIB_PATH "/usr/lib/libinproc_inject.so"
 #define INPROC_AUDIT_ENV_VAR "INPROC_AUDIT_LIB_PATH"
-#define INPROC_DEFAULT_AUDIT_LIB_PATH "build/libinproc_audit.so"
+#define INPROC_DEFAULT_AUDIT_LIB_PATH "/usr/lib/libinproc_audit.so"
 
 #define INPROC_PRELOAD_ENV_VAR "INPROC_PRELOAD_LIB_PATH"
-#define INPROC_DEFAULT_PRELOAD_LIB_PATH "build/libinproc_preload.so"
+#define INPROC_DEFAULT_PRELOAD_LIB_PATH "/usr/lib/libinproc_preload.so"
 
 #define INPROC_NAIVEPATCH_ENV_VAR "INPROC_NAIVEPATCH_LIB_PATH"
-#define INPROC_DEFAULT_NAIVEPATCH_LIB_PATH "build/libinproc_naivepatch.so"
+#define INPROC_DEFAULT_NAIVEPATCH_LIB_PATH "/usr/lib/libinproc_naivepatch.so"
+
